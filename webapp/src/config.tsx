0bb7e8c74760cb16db3282eaa50bb09c39786247
sam-butcher <samuel@typedb.com>
2025-07-22 16:27:43 +0100

    undo a mistake

--- webapp/src/config.tsx
+++ webapp/src/config.tsx
@@ -1,4 +1,4 @@
-export const TYPEDB_ADDRESS = "https://uurk53-0.cluster.typedb.dev:80";
+export const TYPEDB_ADDRESS = "localhost:8000";
 export const TYPEDB_USERNAME = "admin";
 export const TYPEDB_PASSWORD = "password";
-export const TYPEDB_DATABASE = "default";
+export const TYPEDB_DATABASE = "social-network";
